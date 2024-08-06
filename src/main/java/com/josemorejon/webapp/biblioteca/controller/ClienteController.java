d34210bcc8aae01e518e0739e53600af4005640a
Informatica <informatica@alumno.local>
2024-08-06 11:55:42 -0600

    Se agrego el crud de Clientes

--- src/main/java/com/josemorejon/webapp/biblioteca/controller/ClienteController.java
+++ src/main/java/com/josemorejon/webapp/biblioteca/controller/ClienteController.java
@@ -15,8 +15,11 @@ import com.josemorejon.webapp.biblioteca.service.ClienteService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
@@ -34,6 +37,16 @@ public class ClienteController{
         return clienteService.listarClientes();
     }
 
+    @GetMapping("/{dpi}")
+    public ResponseEntity<Cliente> buscarClientePorId(@PathVariable Long dpi){
+        try {
+            Cliente cliente = clienteService.buscarClientePorId(dpi);
+            return ResponseEntity.ok(cliente);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(null);
+        }
+    }
+
     @PostMapping("/")
     public ResponseEntity<Map<String,String >> agregarCliente(@RequestBody Cliente cliente) {
         Map<String,String> response = new HashMap<>();
@@ -44,6 +57,43 @@ public class ClienteController{
         } catch (Exception e) {
             response.put("message" ,"error" );
             response.put("err" ,"No se ha agregado el Cliente" );
+            return ResponseEntity.badRequest().body(response);
+        }
+    }
+
+    @PutMapping("/{dpi}")
+    public ResponseEntity <Map<String, String>> editarCliente(@PathVariable Long dpi, @RequestBody Cliente clienteNew){
+        Map<String,String> response = new HashMap<>();
+        try {
+            Cliente cliente = clienteService.buscarClientePorId(dpi);
+            cliente.setNombreCliente(clienteNew.getNombreCliente());
+            cliente.setApellidoCliente(clienteNew.getApellidoCliente());
+            cliente.setTelefono(clienteNew.getTelefono());
+            clienteService.guardarCliente(cliente);
+            response.put("message", "Se he modificado correctamente");
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            response.put("message" ,"error" );
+            response.put("err" ,"No se ha modificado con exito" );
+            return ResponseEntity.badRequest().body(response);
+        }
+
+
+    }
+
+    @DeleteMapping("/{dpi}")
+    public ResponseEntity<Map<String, String>> eliminarCliente(@PathVariable Long dpi){
+        Map<String, String> response = new HashMap<>();
+        try {
+            Cliente cliente = clienteService.buscarClientePorId(dpi);
+
+            clienteService.eliminarCliente(cliente);
+            response.put("message", "Se ha elimnado con exito");
+            return ResponseEntity.ok(response);
+
+        } catch (Exception e) {
+            response.put("message" ,"error" );
+            response.put("err" ,"No se ha eliminado con exito" );
             return ResponseEntity.badRequest().body(response);
         }
     }
